eba80a8dc9c7c8936dd3a695594b754c1eaf0680
gensmusic <gensmusic@163.com>
2015-03-17 17:53:09 +0800

    osip

--- third_party/osip/osip.cpp
+++ third_party/osip/osip.cpp
@@ -18,7 +18,10 @@
 
 static int g_socket_fd = -1;
 static int g_send_fd = -1;
+static osip_t* g_osip = NULL;
 static const size_t BUFFSIZE = 1600;  // MTU usually doesn't exceed 1600
+static const unsigned MESSAGE_ENTRY_MAX_LENGTH = 256;
+static const unsigned MAX_ADDR_STR = 128;
 
 void exit_if_failed(int ret, const std::string& info) {
     if (ret != 0) {
@@ -37,6 +40,16 @@ void cleanup_socket() {
         close(g_send_fd);
         g_send_fd = -1;
     }
+}
+
+void process_new_request(osip_t* osip, osip_event_t* evt)
+{
+    osip_transaction_t *tran;
+    osip_transaction_init(&tran, IST, osip, evt->sip);
+    //osip_transaction_set_in_socket (tran, socket);
+    osip_transaction_set_out_socket (tran, g_send_fd);
+    osip_transaction_set_your_instance(tran, osip);// store osip in transaction for later usage.
+    osip_transaction_add_event(tran, evt);
 }
 
 void* transport_fun(void* arg) {
@@ -64,7 +77,7 @@ void* transport_fun(void* arg) {
         int rc = osip_find_transaction_and_add_event(osip, evt);
         if(0 != rc) {
             std::cout << "this event has no transaction, create a new one.";
-            // ProcessNewReq(osip, evt);
+            process_new_request(osip, evt);
         }
     }
 
@@ -75,10 +88,7 @@ void* transport_fun(void* arg) {
 // This function, which is implementation of transportation ourself, is used to send message.
 int cb_send_message(osip_transaction_t * tr, osip_message_t * sip, char *host, int port, int out_socket);
 
-void cb_ict_kill_transaction(int type, osip_transaction_t * tr);
-void cb_ist_kill_transaction(int type, osip_transaction_t * tr);
-void cb_nict_kill_transaction(int type, osip_transaction_t * tr);
-void cb_nist_kill_transaction(int type, osip_transaction_t * tr);
+void cb_xixt_kill_transaction(int type, osip_transaction_t * tr);
 
 void cb_transport_error(int type, osip_transaction_t * tr, int error);
 
@@ -168,13 +178,126 @@ int init_net(bool as_server)
     return fd;
 }
 
+int send_invite()
+{
+    static int g_seq_num = 1;
+    char* seq_num_str = NULL;
+    char temp[MESSAGE_ENTRY_MAX_LENGTH] = { 0 };
+    char sip_port[MESSAGE_ENTRY_MAX_LENGTH] = { 0 };
+    char expires[10];
+
+    osip_call_id_t* call_id_ptr;
+    osip_cseq_t* cseq_ptr;
+    osip_message_t* invite_msg;
+
+    int status = osip_message_init(&invite_msg);
+    if (status != 0) {
+        std::cout << "osip message init failed" << std::endl;
+        return -1;
+    }
+    
+    osip_message_set_method(invite_msg, osip_strdup("INVITE"));
+    osip_uri_init(&(invite_msg->req_uri));
+    if ((status = osip_uri_parse(invite_msg->req_uri, "sip:10.10.10.1")) != 0) {
+        std::cout << "osip uri parse failed" << std::endl;
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    osip_message_set_version(invite_msg, osip_strdup("SIP/2.0"));
+    osip_message_set_status_code(invite_msg, 0);
+    osip_message_set_reason_phrase(invite_msg, NULL);
+
+    osip_message_set_to(invite_msg, "sip:Hal@10.10.10.2");
+    osip_message_set_from(invite_msg, "sip:dafei@10.10.10.3");
+
+    if ((status = osip_call_id_init(&call_id_ptr)) != 0) {
+        std::cout << "call id failed" << std::endl;
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    char* call_id_number_str = (char*)osip_malloc(MAX_ADDR_STR);
+    unsigned number = osip_build_random_number();
+    sprintf(call_id_number_str, "%u", number);
+    osip_call_id_set_number(call_id_ptr, call_id_number_str);
+
+    osip_call_id_set_host(call_id_ptr, osip_strdup("10.1.1.63"));
+
+    invite_msg->call_id = call_id_ptr;
+
+    if ((status = osip_cseq_init(&cseq_ptr)) != 0) {
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    g_seq_num++;
+    seq_num_str = (char*)osip_malloc(MAX_ADDR_STR);
+    sprintf(seq_num_str, "%i", g_seq_num);
+    osip_cseq_set_number(cseq_ptr, seq_num_str);
+    osip_cseq_set_method(cseq_ptr, osip_strdup("INVITE"));
+    invite_msg->cseq = cseq_ptr;
+
+    osip_message_set_max_forwards(invite_msg, "70");
+    sprintf(temp, "SIP/2.0/%s %s;branch=z9hG4bK%u", "UDP", "10.10.10.3", osip_build_random_number());
+    osip_message_set_via(invite_msg, temp);
+
+    osip_message_set_contact(invite_msg, "dafei@10.10.10.3");
+    sprintf(expires, "%i", 3600);
+    osip_message_set_expires(invite_msg, expires);
+
+    osip_message_set_content_length(invite_msg, "0");
+    osip_message_set_user_agent(invite_msg, "TotalView 1.0");
+
+    // osip_message_set_allow(msgPtr, "INVITE");
+    // osip_message_set_allow(msgPtr, "INFO");
+    // osip_message_set_allow(msgPtr, "ACK");
+    // osip_message_set_allow(msgPtr, "CANCEL");
+    // osip_message_set_allow(msgPtr, "BYE");
+
+    osip_transaction_t* transcation;
+    osip_event_t* event;
+    if ((status = osip_transaction_init(&transcation, ICT, g_osip, invite_msg)) != 0) {
+        std::cout << "Failed to init transportation :" << status << std::endl;
+        return -1; 
+    }
+
+    if ((event = osip_new_outgoing_sipmessage(invite_msg)) == NULL) {
+        std::cout << "Can't allocate message" << std::endl;
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    event->transactionid = transcation->transactionid;
+
+    if((status = osip_message_force_update(invite_msg)) != 0){
+        std::cout << "Failed force update:" << status << std::endl;
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    if((status = osip_transaction_add_event(transcation, event)) != 0){
+        std::cout << "Can't add event" << std::endl;
+        osip_message_free(invite_msg);
+        return -1;
+    }
+
+    return 0;
+}
+
 void* command_fun(void* arg)
 {
     std::cout << "command thread is running" << std::endl;
-    std::string buf;
+    std::string command;
     std::cout << ">";
-    while (std::getline(std::cin, buf)) {
-        std::cout << "command is:" << buf << std::endl;
+    while (std::getline(std::cin, command)) {
+        if (command == "invite") {
+            assert(g_osip);
+            send_invite();
+        } else {
+            std::cout << "unknown command:" << command << std::endl;
+        }
+
         std::cout << ">" << std::flush;
     }
     return NULL;
@@ -207,15 +330,16 @@ int main(int argc, char** argv){
     osip_t* osip = NULL;
     ret = osip_init(&osip);
     exit_if_failed(ret, "Failed to init osip");
+    g_osip = osip;
 
     // set callbacks.
     // callback called when a SIP message must be sent.
     osip_set_cb_send_message(osip, &cb_send_message);
     // callback called when a SIP transaction is TERMINATED.
-    osip_set_kill_transaction_callback(osip ,OSIP_ICT_KILL_TRANSACTION, &cb_ict_kill_transaction);
-    osip_set_kill_transaction_callback(osip ,OSIP_IST_KILL_TRANSACTION, &cb_ist_kill_transaction);
-    osip_set_kill_transaction_callback(osip ,OSIP_NICT_KILL_TRANSACTION, &cb_nict_kill_transaction);
-    osip_set_kill_transaction_callback(osip ,OSIP_NIST_KILL_TRANSACTION, &cb_nist_kill_transaction);
+    osip_set_kill_transaction_callback(osip ,OSIP_ICT_KILL_TRANSACTION, &cb_xixt_kill_transaction);
+    osip_set_kill_transaction_callback(osip ,OSIP_IST_KILL_TRANSACTION, &cb_xixt_kill_transaction);
+    osip_set_kill_transaction_callback(osip ,OSIP_NICT_KILL_TRANSACTION, &cb_xixt_kill_transaction);
+    osip_set_kill_transaction_callback(osip ,OSIP_NIST_KILL_TRANSACTION, &cb_xixt_kill_transaction);
     //callback called when the callback to send message have failed.
     osip_set_transport_error_callback(osip ,OSIP_ICT_TRANSPORT_ERROR, &cb_transport_error);
     osip_set_transport_error_callback(osip ,OSIP_IST_TRANSPORT_ERROR, &cb_transport_error);
@@ -262,6 +386,7 @@ int main(int argc, char** argv){
         osip_timers_ist_execute(osip);
         osip_timers_nict_execute(osip);
         osip_timers_nist_execute(osip);
+        // This is not block. We need to block it. Use select.
     }
 
     pthread_join(th_command, NULL);
@@ -287,62 +412,76 @@ int cb_send_message(osip_transaction_t * tr, osip_message_t * sip, char *host, i
     return OSIP_SUCCESS;
 }
 
-void cb_ict_kill_transaction(int type, osip_transaction_t * tr)
+void cb_xixt_kill_transaction(int type, osip_transaction_t * tr)
 {
+    std::cout << __FUNCTION__ << " ";
+    switch (type) {
+    case OSIP_ICT_KILL_TRANSACTION:
+        std::cout << "OSIP_ICT_KILL_TRANSACTION ";
+        break;
+    case OSIP_IST_KILL_TRANSACTION:
+        std::cout << "OSIP_IST_KILL_TRANSACTION ";
+        break;
+    case OSIP_NICT_KILL_TRANSACTION:
+        std::cout << "OSIP_NICT_KILL_TRANSACTION ";
+        break;
+    case OSIP_NIST_KILL_TRANSACTION:
+        std::cout << "OSIP_NIST_KILL_TRANSACTION ";
+        break;
+    default:
+        assert(false); // not reachable.
+        break;
+    }
 
-}
+    std::cout << ", transactionid=" << tr->transactionid << std::endl;
 
-void cb_ist_kill_transaction(int type, osip_transaction_t * tr)
-{
-
-}
-
-void cb_nict_kill_transaction(int type, osip_transaction_t * tr)
-{
-
-}
-
-void cb_nist_kill_transaction(int type, osip_transaction_t * tr)
-{
+    // First, remove transacation.
+    osip_t *osip = (osip_t*) osip_transaction_get_reserved1 (tr);
+    assert(osip == g_osip);
+    int i = osip_remove_transaction(osip, tr);
+    if (i != 0) {
+        std::cout << "osip_remove_transacation failed:" << i
+                << ", transcationid=" << tr->transactionid << std::endl;
+    }
 
 }
 
 void cb_transport_error(int type, osip_transaction_t * tr, int error)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv1xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv2xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv3xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv4xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv5xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcv6xx(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
 
 void cb_rcvreq(int type, osip_transaction_t * tr, osip_message_t * sip)
 {
-
+    std::cout << __FUNCTION__ << std::endl;
 }
