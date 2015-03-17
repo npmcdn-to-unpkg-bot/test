c82f2c5ad5427a47a2986850c95935a164fbba38
gensmusic <gensmusic@163.com>
2015-03-17 22:20:39 +0800

    04

--- third_party/php/xphp/xphp/index.php
+++ third_party/php/xphp/xphp/index.php
@@ -7,5 +7,11 @@
 	if ($_controller == "" || in_array($_controller, explode(",", XPHP_FORBIDDEN_TYPE))) exit();
 
 	// 加载全局函数库
-	include(XPHP_PATH."/common/functions.inc");
+	include(XPHP_PATH."/common/functions.inc");	
+	// 加载controller父类 _Master
+	include(XPHP_PATH."/mvc/controller/_Master.inc");
+	
+	include(XPHP_PATH."/mvc/controller/".$_controller.".inc");
+	$_init_controller = new $_controller(); // == $_init_controller = new index(); 因为我没加载这个类了.不过是以变量的形式
+	$_init_controller->$_action(); // 执行类方法
 ?>
