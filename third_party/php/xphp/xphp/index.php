273fdbc433faed58064bb7825ae6390e36b2d46d
gensmusic <gensmusic@163.com>
2015-03-17 22:05:39 +0800

    l 03

--- third_party/php/xphp/xphp/index.php
+++ third_party/php/xphp/xphp/index.php
@@ -1,4 +1,11 @@
 <?php
 	include("xphp.conf");
-	include(XPHP_PATH."/mvc/view/".XPHP_VIEWPATH."/index.php");
+//	include(XPHP_PATH."/mvc/view/".XPHP_VIEWPATH."/index.php");
+
+	$_controller = isset($_GET["controller"]) ? $_GET["controller"] : ""; // 获取controller参数
+	$_action = isset($_GET["action"]) ? $_GET["action"] : "";	// 获取action参数,这个参数可以没有,没有的话会执行controller的构造函数
+	if ($_controller == "" || in_array($_controller, explode(",", XPHP_FORBIDDEN_TYPE))) exit();
+
+	// 加载全局函数库
+	include(XPHP_PATH."/common/functions.inc");
 ?>
--- third_party/php/xphp/xphp/xphp/mvc/view/default/index.php
+++ third_party/php/xphp/xphp/xphp/mvc/view/default/index.php
@@ -1,3 +1,4 @@
 <div>头部</div>
 <div>身体</div>
+<?php test() ?>
 <div>尾部</div>
