42eb8f9249e06ddcd32cefbd7232363bcb84a63d
gensmusic <gensmusic@163.com>
2015-04-03 16:50:23 +0800

    1

--- third_party/php/elephant/web-elephant/includefiles/bottom.php
+++ third_party/php/elephant/web-elephant/includefiles/bottom.php
@@ -1,6 +1,31 @@
-<div>苏ICP备14027568号
-©jtthink.com Inc. All Rights Reserved</div>
-  <br /><br />
-  <br /><br />
- </body>
+<?php
+    include "module/page.php";
+?>
+
+<style>
+.bottom {width: 100%; margin: 0 auto; background: #474747; height: 80px; padding-top: 20px;
+    color: #fff
+}
+
+.bottom a{color: #fff; text-decoration:  none;}
+.bottom a:hover{text-decoration: underline;}
+</style>
+
+<div class="bottom">
+    <p>友情链接:
+    <?php
+        $get_links = get_friendLinks();
+        // // var_export($get_links);
+        // foreach ($get_links as $link) {
+        //     echo '<a href="'.$link['href'].'" target="'.$link['target'].'">'.$link['title'].'</a> |';
+        // }
+    ?> 
+    <?php foreach ($get_links as $link): ?>
+    <?php include "tpl/friendlink.tpl"; ?>
+    <?php endforeach;?>
+    </p>
+    <p>苏ICP备14027568号 ©jtthink.com Inc. All Rights Reserved</p>
+</div>
+
+    </body>
 </html>
---
+++ third_party/php/elephant/web-elephant/module/page.php
@@ -0,0 +1,21 @@
+<?php
+    function get_friendLinks()
+    {
+        $link1=array(
+            "title"=>"51cto",
+            "href"=>"www.51cto.com",
+            "target"=>"_blank",
+            );
+
+        $link2=array(
+            "title"=>"jtthink",
+            "href"=>"www.jtthink.com",
+            "target"=>"_blank",
+            );
+        $links[] = $link1;
+        $links[] = $link2;
+
+        return $links;
+    }
+
+?>
