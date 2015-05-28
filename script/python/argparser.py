6b92b4e1ea010a4aaeb54ee24e4185afb6078f1f
gensmusic <gensmusic@163.com>
2015-05-28 10:28:41 +0800

    1

--- script/python/argparser.py
+++ script/python/argparser.py
@@ -2,7 +2,7 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+parser.add_argument("square", help="display a square of a given number", type=int)
 args = parser.parse_args()
-print args.echo
+print args.square**2
 print args
