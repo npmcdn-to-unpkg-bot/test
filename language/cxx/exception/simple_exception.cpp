9d4036da68642e8876f80227944385036b02fa25
gensmusic <gensmusic@163.com>
2015-06-10 17:49:42 +0800

    1

--- language/cxx/exception/simple_exception.cpp
+++ language/cxx/exception/simple_exception.cpp
@@ -4,8 +4,17 @@
 
 int main() {
     try {
-        throw 20;
-    } catch (int e) {
-        std::cout << "exception " << e << std::endl;
+        // throw 20;
+        throw 'a';
+        throw 2.0;
+    }
+    catch (int e) {
+        std::cout << "exception int " << e << std::endl;
+    }
+    catch (char e) {
+        std::cout << "exception char " << e << std::endl;
+    }
+    catch (...) {
+        std::cout << "exception default " << std::endl;
     }
 }
