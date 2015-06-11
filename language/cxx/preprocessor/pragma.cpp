5a91b053c154b364e588c7d024581c3d1af835dd
gensmusic <gensmusic@163.com>
2015-06-11 11:22:16 +0800

    1

---
+++ language/cxx/preprocessor/pragma.cpp
@@ -0,0 +1,13 @@
+// This directive is used to specify diverse options to the compiler. 
+// These options are specific for the platform and the compiler you use. 
+// Consult the manual or the reference of your compiler for more information on the possible parameters that you can define with #pragma.
+
+// If the compiler does not support a specific argument for #pragma, 
+// it is ignored - no syntax error is generated.
+
+ #pragma message "print a message, won't break compling"
+
+int main()
+{
+
+}
