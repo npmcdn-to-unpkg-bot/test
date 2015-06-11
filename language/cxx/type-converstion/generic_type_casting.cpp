d16800ff00d00f29fe1c4b6a170c05de93ec7603
gensmusic <gensmusic@163.com>
2015-06-11 14:08:16 +0800

    1

---
+++ language/cxx/type-converstion/generic_type_casting.cpp
@@ -0,0 +1,20 @@
+
+/*
+Type casting
+
+
+For generic type-casting
+---------------------------
+C++ is a strong-typed language. Many conversions, specially those that imply a different interpretation of the value, r
+equire an explicit conversion, known in C++ as type-casting. 
+There exist two main syntaxes for generic type-casting: 
+    functional and c-like:
+
+*/
+
+double x = 10.3;
+int y;
+y = int (x);    // functional notation
+y = (int) x;    // c-like cast notation 
+
+
