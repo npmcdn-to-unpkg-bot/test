1d328b7e00cb1994d19bff535d3c78562ce1ac8b
gensmusic <gensmusic@163.com>
2015-07-26 14:26:33 +0800

    1

--- l/python/book/c18/args-pass.py
+++ l/python/book/c18/args-pass.py
@@ -59,3 +59,28 @@ def f(a, *pargs, **kargs):
     print a,pargs,kargs
 f(1)
 f(1,2,3,x=1,y=1)
+
+# 解包参数
+def func(a,b,c,d):
+    print a,b,c,d
+args = (1,2)
+args += (3,4)
+func(*args)
+
+args = {'a':1, 'b':2, 'c':3}
+args['d'] = 4
+func(**args)
+
+# 更加灵活的方式
+func(1, *(2,3), **{'d' :4})
+
+# 应用函数通用性
+def tracer(func, *pargs, **kargs):
+    print 'calling:', func.__name__
+    return func(*pargs, **kargs)
+def func(a,b,c,d):
+    print a+b+c+d
+tracer(func, 1, 2, c=3, d=4)
+
+# 废弃的apply == *args, **args
+# func(*pargs, **kargs) == apply(func, pargs, kargs)
