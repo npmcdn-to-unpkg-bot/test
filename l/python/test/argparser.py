fca2777decc68078c2c8f8e41054c456d39294a0
gensmusic <gensmusic@163.com>
2015-07-28 11:06:26 +0800

    1

--- l/python/test/argparser.py
+++ l/python/test/argparser.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python
 
+import sys
 import argparse
+
+# see
+# http://blog.chinaunix.net/xmlrpc.php?r=blog/article&uid=21633169&id=4387657
+
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number", type=int)
+# args = "*" [0, ...] args
+# args = '+' [1, ...) args
+parser.add_argument("first", help="first arg", nargs="?", default="99")
+parser.add_argument("second", help="second", nargs="?", default="88")
+
+
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument("-c", "--choice", help="give me your choice", choices=[0,2,4])
 parser.add_argument("-f", "--filename", help="default action=store, type=string")
 parser.add_argument("-l", "--list", help="test action acount, try -llll to see list value",
                     default = 0, action="count")
+
 args = parser.parse_args()
 
-if args.verbose:
-    print "square of {} is {}".format(args.square, args.square**2)
-else:
-    print args.square**2
+def print_args():
+    if args.first:
+        print 'Fisrt is ',args.first
+    if args.second:
+        print 'Second is ', args.second
 
-print "-----------\n{}".format(args)
+
+for args_list in [ '', '1', '1 2 3 4 5 6']:
+    print 'args:', args_list
+    args = parser.parse_args(args_list.split())
+    print_args()
+
+
+# print "-----------\n{}".format(args)
