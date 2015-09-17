c8679628496cab29a9cc8a370b67beb2bb40dd7a
gensmusic <gensmusic@163.com>
2015-09-17 11:23:04 +0800

    1

---
+++ third_party/python/flask/project-test/flask-http-authentication.py
@@ -0,0 +1,46 @@
+#coding:utf-8
+
+from flask import Flask,request
+from flask.ext.httpauth import HTTPDigestAuth
+
+"""
+pip install flask-httpauth
+
+https://github.com/miguelgrinberg/Flask-HTTPAuth
+
+# use chrome to open http://127.0.0.1:5000
+
+# curl fails to authenticate. Use chrome to open the link 
+curl -u admin:admin http://127.0.0.1:5000 -v --digest
+
+"""
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+auth = HTTPDigestAuth()
+
+users = {
+    "admin" : "admin",
+    "dafei" : "test"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        print 'Found user authorization'
+        return users.get(username)
+    return None
+
+@app.route('/')
+@auth.login_required
+def index():
+    return 'Your authorization name is:%s' % auth.username()
+
+@app.route('/go')
+@auth.login_required
+def go():
+    return 'Your are logged :%s' % auth.username()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
