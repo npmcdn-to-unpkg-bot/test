67d56bd5fb8e057df9d812fdd9cd4470dfef5be9
gensmusic <gensmusic@163.com>
2015-10-22 15:44:42 +0800

    1

--- third_party/python/flask/bootstrap/usage.py
+++ third_party/python/flask/bootstrap/usage.py
@@ -4,9 +4,17 @@ from flask import Flask,request, render_template
 
 app = Flask(__name__)
 
+pages = (
+    ('shange', u'预定义栅格类'),
+    ('less', u'Less mixin 和变量'),
+    ('layout', u'排版'),
+    ('html_body_width_height', u'Html body width height test'),
+    ('center_element', u'center-element')
+)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pages=pages)
 
 @app.route('/shange')
 def shange():
@@ -20,5 +28,12 @@ def less():
 def layout():
     return render_template('layout.html')
 
+@app.route('/html_body_width_height')
+def html_body_width_height():
+    return render_template('html_body_width_height.html')
+
+@app.route('/center-element')
+def center_element():
+    return render_template('center-element.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
