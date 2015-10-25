4b410757c509bfd59cade3fefaa06dfb733118ed
gensmusic <gensmusic@163.com>
2015-10-25 20:50:35 +0800

    1

--- l/python/test/json-to-object/json-to-object.py
+++ l/python/test/json-to-object/json-to-object.py
@@ -24,3 +24,34 @@ p2 = Payload(j2)
 print dir(p2)
 print type(p2.payload)
 print p2.payload
+
+print '-' *50
+
+class ParseBase(object):
+    """docstring for ParseBase"""
+    def __init__(self):
+        super(ParseBase, self).__init__()
+        self.http_status_code = 0
+
+    def parse(self, j):
+        dict_data = json.loads(j)
+        for key in self.__dict__:
+            print 'key:{}'.format(key)
+            if not key.startswith('http'):
+                value = dict_data.get(key)
+                self.__dict__[key] = value
+
+class Http(ParseBase):
+    """docstring for Http"""
+    def __init__(self):
+        super(Http, self).__init__()
+        self.name = None
+        self.id = None
+    
+h = Http()
+h.parse('{ "name" : "大飞", "id":1 }')
+print dir(h)
+print h.http_status_code
+print h.name
+print h.id
+
