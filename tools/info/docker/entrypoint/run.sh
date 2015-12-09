deda41051cb7178e0e7d7460ca93fbebe47edc6a
gensmusic <gensmusic@163.com>
2015-12-09 18:07:27 +0800

    1

--- tools/info/docker/entrypoint/run.sh
+++ tools/info/docker/entrypoint/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 # DO NOT forget above
 
-echo "aaaaaaaaaaaa"
+echo "\$@:$@"
+echo "\$1:$1"
+echo "\$2:$2"
+echo "\$3:$3"
+echo "\$4:$4"
 
 echo "redis ip:"$REDIS_PORT_6379_TCP_ADDR
 
 echo "start supervisord ..."
-supervisord -n
+# supervisord -n
