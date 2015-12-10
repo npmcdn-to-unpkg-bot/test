a0cd95383905a368fd9a49c9387fedcdb5047cb9
gensmusic <gensmusic@163.com>
2015-12-10 16:03:02 +0800

    1

--- third_party/python/PyAPNs/usage.py
+++ third_party/python/PyAPNs/usage.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import os
 import sys
@@ -6,42 +7,53 @@ import logging
 logging.basicConfig()
 
 # config apns's logger
-# logger = logging.getLogger("apns")
-# stream_handler = logging.StreamHandler(sys.stderr)
-# logger.addHandler(stream_handler)
-# logger.setLevel(logging.DEBUG)
+logger = logging.getLogger("apns")
+stream_handler = logging.StreamHandler(sys.stderr)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
-cert_file = os.environ.get('APNS_CERT_FILE')
-key_file = os.environ.get('APNS_KEY_FILE')
-if None in (cert_file, key_file):
-    print 'Please export APNS_CERT_FILE=xxx'
-    print 'Please export APNS_KEY_FILE=xxx'
+_DEFAULT_PUSH_NUMBER = 100
+_DEFAULT_PUSH_MESSAGE = 'this is the {} time pushing'
+_DEFAULT_START_IDENTIFIER = 0
+_DEFAULT_PUSH_INVTERVAL = 1
 
-    exit(1)
-
-apns = APNs(use_sandbox=True,
-        cert_file=cert_file,
-        key_file=key_file,
-        enhanced=True)
+def get_certicate_from_environment():
+    cert_file = os.environ.get('APNS_CERT_FILE')
+    key_file = os.environ.get('APNS_KEY_FILE')
+    if None in (cert_file, key_file):
+        print 'Please use --cert_file or export APNS_CERT_FILE=xxx'
+        print 'Please use --key_file  or export APNS_KEY_FILE=xxx'
+        exit(1)
+    return (cert_file, key_file)
 
 def response_listener(error):
     print 'Get error msg:' + str(error)
 
-apns.gateway_server.register_response_listener(response_listener)
+def send_notification(token_hex=None,
+        times=_DEFAULT_PUSH_NUMBER,
+        interval=_DEFAULT_PUSH_INVTERVAL,
+        start_identifier=_DEFAULT_START_IDENTIFIER,
+        cert_file=None,
+        key_file=None):
+    # apns certificate files
+    if None in (cert_file, key_file):
+        (cert_file, key_file) = get_certicate_from_environment()
 
-def send_notification(times=1, interval=None, start_identifier=100):
+    # create apns
+    apns = APNs(use_sandbox=True, cert_file=cert_file, key_file=key_file, enhanced=True)
+    apns.gateway_server.register_response_listener(response_listener)
+
     start_time = time.time()
     # Send a notification
     for i in range(times):
-        token_hex = '9b95bd9b60ef84a34777f6987163a8e4250e56d815232657284902ef3e78e5a6'
         identifier = start_identifier + i
-        payload = Payload(alert="This is the " + str(i) + " message", sound="default", badge=i)
+        payload = Payload(alert=_DEFAULT_PUSH_MESSAGE.format(i+1), sound="default", badge=i+1)
         apns.gateway_server.send_notification(token_hex, payload, identifier=identifier)
-        print 'send over for identifier:', identifier
+        print '> send over for identifier:<{}>'.format(identifier)
         if interval is not None:
-            print 'sleep for ', interval, ' seconds...'
+            print '>  sleep for {} seconds...'.format(interval)
             time.sleep(interval)
-    print 'Use time:{}'.format(time.time() - start_time)
+    print '\n# Use time:{}\n'.format(time.time() - start_time)
 
 def send_error_notification(times=1, start_identifier=0):
     """
@@ -71,6 +83,19 @@ def send_error_notification(times=1, start_identifier=0):
 # apns.gateway_server.send_notification_multiple(frame)
 
 if __name__ == '__main__':
-    # send_notification(times=1, start_identifier=4294967291)
-    # send_error_notification(times=1, start_identifier=4294967290)
-    send_notification(times=1000, start_identifier=10000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('token', help='You must specify an token')
+    parser.add_argument('-m', '--message', help='push message body, default "{}"'.format(_DEFAULT_PUSH_MESSAGE), default=_DEFAULT_PUSH_MESSAGE)
+    parser.add_argument('-n', '--number', help='push times, default {}'.format(_DEFAULT_PUSH_NUMBER), type=int, default=_DEFAULT_PUSH_NUMBER)
+    parser.add_argument('-i', '--identifier', help='start identifier, default {}'.format(_DEFAULT_START_IDENTIFIER), type=int, default=_DEFAULT_START_IDENTIFIER)
+    parser.add_argument('-I', '--interval', help='push interval, default {} seconds'.format(_DEFAULT_PUSH_INVTERVAL), type=int, default=_DEFAULT_PUSH_INVTERVAL)
+    parser.add_argument('--cert_file', help='apns cert file, you can also export APNS_CERT_FILE=xxx')
+    parser.add_argument('--key_file', help='apns key file, you can also export APNS_KEY_FILE=xxx')
+
+    args = parser.parse_args()
+
+    send_notification(args.token, times=args.number,
+            interval=args.interval,
+            start_identifier=args.identifier,
+            cert_file=args.cert_file,
+            key_file=args.key_file)
