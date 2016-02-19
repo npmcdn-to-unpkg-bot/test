23ed1fecb93b5d5d5f85992058df16ab84f5b44e
gensmusic <gensmusic@163.com>
2016-02-19 18:12:05 +0800

    1

---
+++ third_party/meteor/simple-todos/simple-todos.js
@@ -0,0 +1,42 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+    tasks : function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event) {
+      console.log(event);
+      event.preventDefault();
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+    }
+  });
+  Template.task.events({
+    "click .toggle-checked" : function(event) {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete" : function() {
+      Tasks.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
