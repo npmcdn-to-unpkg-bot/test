25f6fd62b815718296abf3b288fbfa8b238f5fd6
gensmusic <gensmusic@163.com>
2016-03-08 10:43:15 +0800

    1

--- third_party/meteor/microscope/server/fixtures.js
+++ third_party/meteor/microscope/server/fixtures.js
@@ -1,4 +1,11 @@
 if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+  // create two users
+  var tomId = Meteor.users.insert({
+    profile: {name: 'Tom Coleman'}
+  });
+
   Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/Introducing-telescope/'
