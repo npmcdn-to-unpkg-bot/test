ef384d39f441ea451c6a2f262c6d5ad02bb5f356
gensmusic <gensmusic@163.com>
2016-03-10 11:52:41 +0800

    1

--- l/go/go-programing/c02/sorter/src/sorter/sorter.go
+++ l/go/go-programing/c02/sorter/src/sorter/sorter.go
@@ -6,6 +6,10 @@ import "io"
 import "os"
 import "strconv"
 import "bufio"
+import "time"
+
+import "algorithm/bubblesort"
+import "algorithm/qsort"
 
 var infile *string = flag.String("i", "infile", "File contains values for sorting")
 var outfile *string = flag.String("o", "outfile", "File to receive sorted values")
@@ -47,16 +51,44 @@ func readValues(infile string)(values []int, err error) {
     return
 }
 
+func writeValues(values []int, outfile string) error {
+    file, err := os.Create(outfile)
+    if err != nil {
+        fmt.Println("Failed to create the outfile ", outfile)
+        return err
+    }
+
+    defer file.Close()
+
+    for _, value := range values {
+        str := strconv.Itoa(value)
+        file.WriteString(str + "\n")
+    }
+    return nil
+}
+
 func main() {
     flag.Parse()
 
     if infile != nil {
-        fmt.Println("infile = ", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+        fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =",
+*algorithm)
     }
 
     values, err := readValues(*infile)
     if err == nil {
-        fmt.Println("Read values:", values)
+        t1 := time.Now()
+        switch *algorithm {
+        case "qsort":
+            qsort.QuickSort(values)
+        case "bubblesort":
+            bubblesort.BubbleSort(values)
+        default:
+            fmt.Println("Sorting algorithm", *algorithm, "is unsupported")
+        }
+        t2 := time.Now()
+        fmt.Println("The sorting process costs", t2.Sub(t1))
+        writeValues(values, *outfile)
     } else {
         fmt.Println(err)
     }
