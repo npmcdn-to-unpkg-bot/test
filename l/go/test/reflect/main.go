b8a89c5658199db7decb78847686b46c96a28d7e
gensmusic <gensmusic@163.com>
2016-03-14 21:58:02 +0800

    1

---
+++ l/go/test/reflect/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var x float64 = 3.4
+	v := reflect.ValueOf(x)
+	fmt.Println("type:", v.Type())
+	fmt.Println("kind:", v.Kind())
+	fmt.Println("value:", v.Float())
+}
