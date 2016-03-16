09ded098cee4d04d06cf3633daf4d2e547c8578c
gensmusic <gensmusic@163.com>
2016-03-16 17:11:52 +0800

    1

---
+++ l/go/test/testing/gotest.go
@@ -0,0 +1,11 @@
+package gotest
+
+import "errors"
+
+func Division(a, b float64) (float64, error) {
+    if b == 0 {
+        return 0, errors.New("divide by zero")
+    }
+
+    return a/b, nil
+}
