52dd4cd2d923d73167bc39c3c462b41ae687776f
gensmusic <gensmusic@163.com>
2016-04-18 16:23:18 +0800

    1

--- third_party/nodejs/third-party/moment/index.js
+++ third_party/nodejs/third-party/moment/index.js
@@ -6,3 +6,8 @@ console.log(moment().format('YYYY-MM-DD'))
 console.log(moment().format('YYYYMMDD'))
 
 console.log(moment({day: 1}).format('YYYY-MM-DD'))
+
+console.log(moment().format('YYYY年MM月'))
+console.log(moment().format('DD'))
+console.log(moment({day:11}).format('D'))
+console.log(moment({day:1}).format('D'))
