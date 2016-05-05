1e4ef92e93da31159231fc1f03b97f2558e6967f
gensmusic <gensmusic@163.com>
2016-05-05 17:37:40 +0800

    1

--- l/javascript/regexp/index.js
+++ l/javascript/regexp/index.js
@@ -58,3 +58,33 @@ while (r = tr.exec(s)) {
 //     console.log(r[1]);
 // }
 
+
+console.log('-------------------------')
+var s1 = `'1234', "345"`;
+var re1 = /\'[\d]{4}\'.*/;
+console.log(re1.exec(s1))
+
+console.log('-------------------------')
+// 不知道这种应该咋写
+var re2 = new RegExp("\\\'[\\d]{4}\\\'.*/")
+console.log(re2.exec(s1))
+
+console.log('-------------------------')
+var re3 = new RegExp(/\'[\d]{4}\'.*/)
+console.log(re3.exec(s1))
+
+//惰性匹配,匹配最小的
+// 惰性模式的语法很简单，即是在贪婪模式后面加上一个“?”即可
+// * –> *?
+// + –> +?
+// {n,} –> {n,}?
+console.log('-------------------------')
+var s2 = `<html>
+    <p>aaa</p><p>bbb</p><p>ccc</p>
+<html>`
+var re4 = /<p>(.*)<\/p>/;
+console.log(re4.exec(s2)[1]);
+console.log('-------------------------')
+var re5 = /<p>(.*?)<\/p>/;
+console.log(re5.exec(s2)[1]);
+
