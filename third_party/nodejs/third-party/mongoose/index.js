4c4fffd69323bcd8901ea485751f452bdf0b19e2
gensmusic <gensmusic@163.com>
2016-05-30 18:15:42 +0800

    1

--- third_party/nodejs/third-party/mongoose/index.js
+++ third_party/nodejs/third-party/mongoose/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/my_database');
@@ -16,18 +18,67 @@ var Comment = new Schema({
 var BlogPost = new Schema({
     title     : { type: String, default: 'War is coming' },
     date      : { type: Date, default: Date.now },
-    comments: [Comment]
+    comments: [ {
+        month: {type: String},
+        comments: [Comment]
+    }]
 });
 
 var BlogPostModel = mongoose.model('BlogPost', BlogPost)
 
-var post = new BlogPostModel()
-post.title = 'a test one';
-post.comments.push({name: 'bob', age: 20})
-post.save(function(err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('save ok')
-    }
-})
+
+function insertCommnets(title, month, comments) {
+
+
+        BlogPostModel.findOne({title: title}, function(err, post) {
+            if (err) {
+                console.log(err)
+            } else {
+                if (!post) {
+                    console.log('no post')
+                    post = new BlogPostModel({
+                        title: 'War is coming',
+                        comments: [
+                            {month: month, comments}
+                        ]
+                    })
+                    post.save(function(err) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.log('save ok')
+                        }
+                    })
+                } else {
+                    var index = -1;
+                    for (let i = 0; i < post.comments.length; i++) {
+                        if (post.comments[i].month === month) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    if (index === -1) {
+                        console.log('No month: ' + month);
+                        post.comments.push({month:month, comments})
+                    }else {
+                        console.log('has month: ' + month)
+                        for (let i of comments) {
+                            post.comments[index].comments.push(i)
+                        }
+                    }
+                    post.save(function(err) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.log(`month:${month} save ok`)
+                        }
+                    })      
+                }
+            }
+        })
+}
+
+insertCommnets('War is coming', '201601', [{name: 'Lily', age: 22}, {name: 'Lucy', age: 23}])
+setTimeout(insertCommnets, 1000, 'War is coming', '201601', [{name: 'Bob', age: 22}, {name: 'John', age: 23}])
+setTimeout(insertCommnets, 2000, 'War is coming', '201602', [{name: 'Bob', age: 22}, {name: 'John', age: 23}])
+
